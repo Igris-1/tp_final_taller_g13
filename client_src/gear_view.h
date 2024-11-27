068c418ef9c9f6753878464b50cc0030f84cfad2
FedericoMPeralta <fperalta@fi.uba.ar>
2024-11-27 16:31:45 -0300

    se ven bien los cascos y armaduras

--- client_src/gear_view.h
+++ client_src/gear_view.h
@@ -21,18 +21,20 @@ class GearView {
 private:
     Renderer& renderer;
     std::vector<Texture>& gear_textures;
-    std::vector<Texture>& accessories_sprites;
+    std::vector<Texture>& accessories_textures;
     std::vector<std::vector<int>> gear_frames;
+    std::vector<std::vector<int>> accessories_frames;
+
     void push_gear_frame_values();
     void push_gear_frame_for_idle_walking_duck();
     void push_gear_frame_for_jumping_falling_duck();
     void push_gear_frame_for_flying_duck();
     void push_gear_frame_for_crouching_duck();
-    void draw_accessories(duck_DTO duck, bool facing_direction, int gear_id, int action);
+    void draw_accessories(duck_DTO duck, bool facing_direction, int action);
     void draw_gear_in_hands(int x, int y, int gear, int inclination, int facing_direction);
 
 public:
-    GearView(Renderer& renderer, std::vector<Texture>& gear_textures, std::vector<Texture>& accessories_sprites);
+    GearView(Renderer& renderer, std::vector<Texture>& gear_textures, std::vector<Texture>& accessories_textures);
     void draw_held_gear(bool& facing_direction, duck_DTO& duck);
     void draw_gear(weapon_DTO& weapon);
     ~GearView();
